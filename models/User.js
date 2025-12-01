a10639208efda5ce9962e98349036bef88d74874
sakshamjain270329-commits <sakshamjain270329@gmail.com>
2025-12-01 11:04:08 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    username: String,
+    email: String,
+    password: String
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
